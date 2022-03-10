ad4dea99c8f116a3b8fea2e7ec69806b17bc87ac
efuraimuJS <efuraimu.js@gmail.com>
2022-03-10 07:14:52 -0800

    tasks 0 - 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
   printf("Size of a char: %d byte(s)\n", sizeof(char));
   printf("Size of a int: %d byte(s)\n", sizeof(int));
   printf("Size of a long int: %d byte(s)\n", sizeof(long int));
