d2e2edd612ac185598b6b9b19803c3b85ced130a
efuraimuJS <efuraimu.js@gmail.com>
2022-03-10 02:21:40 -0800

    Task 0 - 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+  -->The main() function is the first function in your program that is executed when it begins executing
+  --> The puts function in C is used to write a line or string to the stdout that is up to, but does not include, the null character
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+  --> The printf() is a library function to send formatted output to the screen.
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
