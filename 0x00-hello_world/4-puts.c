c9617308d4e63859199f249a2384e76a6604d63d
efuraimuJS <efuraimu.js@gmail.com>
2022-03-10 07:43:56 -0800

    tasks 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
-  -->The main() function is the first function in your program that is executed when it begins executing
-  --> The puts function in C is used to write a line or string to the stdout that is up to, but does not include, the null character
- */
 
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
