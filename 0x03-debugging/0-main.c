6df3ead1e2281492f3e71358c8dcbc0d84de6f70
efuraimuJS <efuraimu.js@gmail.com>
2022-03-17 07:15:10 -0700

    Debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - tests function prints if integer positive or negative
 * Return: 0
 */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	 i = 0;
 	positive_or_negative(i);
 
 	return (0);
