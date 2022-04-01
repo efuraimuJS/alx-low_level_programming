a4f6e073d9524d3c9837fd4e8866759e62ded39b
efuraimuJS <efuraimu.js@gmail.com>
2022-04-01 11:37:13 -0700

    Advance Tasks

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,2 +1,11 @@
-int _isupper(int c);
+#include "main.h"
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
