e537297497dc09462e12773ac80a524d6f7178be
efuraimuJS <efuraimu.js@gmail.com>
2022-04-21 16:23:09 -0700

    Basic & Advance Tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,42 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
+
 /**
- * add_node_end - add a new node at the end of `list_t` list
- * @head: double pointer to head
- * @str: string to duplicate into new node
- * Return: Address of the new element or NULL if failed
- */
+  * add_node_end - fills memory with a constant byte
+  * @head: is the owner of the dog
+  * @str: is a string
+  * Return: a address
+  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current;
-	list_t *new_node;
-	int c;
+	list_t *new, *headcopy;
+	size_t n;
 
-	current = *head;
-	while (current && current->next != NULL)
-		current = current->next;
+	headcopy = *head;
 
-	for (c = 0; dup_str[c] != '\0'; c++)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (n = 0; new->str[n] != '\0'; n++)
 		;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new->len = n;
+	if (*head == NULL)
 	{
-		free(new_node);
-		return (NULL);
+		new->next = *head;
+		*head = new;
 	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = NULL;
-
-	if (current)
-		current->next = new_node;
 	else
-		*head = new_node;
-	return (new_node);
+	{
+		while (headcopy->next != NULL)
+			headcopy = headcopy->next;
+		new->next = (headcopy)->next;
+		(headcopy)->next = new;
+	}
+
+	return (*head);
 }
