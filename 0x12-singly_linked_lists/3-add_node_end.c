9e6c871e801216682ba15a845fb36bb3ad20f589
efuraimuJS <efuraimu.js@gmail.com>
2022-04-21 16:10:36 -0700

    Basic & Advance Tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,79 +1,43 @@
-#include <stdlib.h>
 #include "lists.h"
-
 /**
- * add_node_end - function with two arguments
- * @head: pointer to struct of linked list
- * @str: char type pointer to string
- *
- * Description: adds a new node at the end of linked list
- * Return: address of new element
- */
+ * add_node_end - Add a new node at the end of a list.
+ * @head: Address of the first node of a list.
+ * @str: Address of the string to insert into the new node.
+ * Return: Address of the new node.
+ **/
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int count = 0;
-	list_t *end_node, *cursor;
-
-	end_node = malloc(sizeof(list_t));
-	if (end_node == NULL)
-		return (NULL);
-
-	if (str)
-	{
-		end_node->str = _strdup(str);
-		while (str[count] != '\0')
-			count++;
-		end_node->len = count;
-	}
-	else
-	{
-		end_node->str = NULL;
-		end_node->len = 0;
-	}
-	end_node->next = NULL;
-	if (*head)
-	{
-	cursor = *head;
-	while (cursor->next != NULL)
-		cursor = cursor->next;
-	cursor->next = end_node;
-	}
-	else
-		*head = end_node;
-	return (end_node);
-}
-
-/**
- * *_strdup - function with one argument
- * @str: string argument
- *
- * Description: returns a pointer to allocated space in memory
- * Return: pointer
- */
-char *_strdup(const char *str)
-{
-	int i, j;
-	char *ptr;
+	list_t *temp, *temp2;
+	unsigned int length = 0;
 
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (*(str + i) != '\0')
-	{
-		i++;
-	}
 
-	ptr = malloc(sizeof(char) * i + 1);
-
-	if (ptr == NULL)
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
 		return (NULL);
 
-	j = 0;
-	while (str[j] != '\0')
+	temp->str = strdup(str);
+	if (temp->str == NULL)
 	{
-		ptr[j] = str[j];
-		j++;
+		free(temp);
+		return (NULL);
 	}
-	ptr[j] = '\0';
-	return (ptr);
+	while (str[length])
+		length++;
+	temp->len = length;
+	temp->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+
+	temp2 = *head;
+	while (temp2->next)
+		temp2 = temp2->next;
+	temp2->next = temp;
+	return (temp);
 }
