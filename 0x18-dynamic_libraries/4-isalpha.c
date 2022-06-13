de8fcbe88f2269ce78a67ae48642350c445c7613
efuraimuJS <96207346+efuraimuJS@users.noreply.github.com>
2022-06-13 23:54:10 +0300

    adding .c files resources

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is a letter
+ * @c: the character to test
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
